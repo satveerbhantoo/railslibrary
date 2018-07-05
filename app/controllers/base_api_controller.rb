ac5510beaba013fd87a8272a4d417d243fc77126
satveerbhantoo011@gmail.com <satveerbhantoo011@gmail.com>
2018-07-05 14:19:06 +0400

    base api error

--- app/controllers/base_api_controller.rb
+++ app/controllers/base_api_controller.rb
@@ -1,7 +1,7 @@
 class BaseApiController < ApplicationController
 
     ##before_action :parse_request, :authenticate_user_from_token!
-    skip_before_filter :verify_authenticity_token  
+    skip_before_action :verify_authenticity_token  
     
     private 
         def authenticate_user_from_token!
